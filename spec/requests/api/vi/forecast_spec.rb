ca4006eb936d646b345bea38894feaf2da6783ac
Caleb Thomas <cjthomas00@comcast.net>
2023-04-22 10:00:46 -0600

    write test for US1

--- spec/requests/api/vi/forecast_spec.rb
+++ spec/requests/api/vi/forecast_spec.rb
@@ -1,5 +1,23 @@
 require "rails_helper"
 
 RSpec.describe "Forecast API" do
-  
+  before :each do
+    get "api/v1/forecast?location=cincinnati,oh"
+    @parsed_data = JSON.parse(response.body, symbolize_names: true)
+  end
+
+  it "returns data" do
+    expect(@parsed_data).to be_a(Hash)
+    expect(@parsed_data).to have_key(:data)
+  end
+
+  it "has id, type, and attributes" do
+    expect(@parsed_data[:data]).to have_key(:id)
+    expect(@parsed_data[:data][:id]).to eq(null)
+
+    expect(@parsed_data[:data]).to have_key(:type)
+    expect(@parsed_data[:data][:type]).to eq("forecast")
+
+    expect(@parsed_data[:data]).to have_key(:attributes)
+  end
 end
