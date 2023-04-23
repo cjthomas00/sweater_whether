bac9649932a5fb68329f7a488d727aee7d16cc21
Caleb Thomas <cjthomas00@comcast.net>
2023-04-23 15:05:58 -0600

    add sessions controller.

---
+++ app/controllers/api/v0/sessions_controller.rb
@@ -0,0 +1,24 @@
+class Api::V0::SessionsController < ApplicationController
+  before_action :check_fields
+
+  def create
+    user = User.find_by(email: user_params[:email])
+    if user && user.authenticate(user_params[:password])
+      render json: UserSerializer.new(user), status: 200
+    else 
+      render json: { error: "Invalid credentials" }, status: 401
+    end
+  end
+
+  private 
+
+  def user_params
+    params.permit(:email, :password)
+  end
+
+  def check_fields
+    if user_params[:email] == "" || user_params[:password] == ""
+      render json: {error: "1 or More Fields are Missing"}, status: 401
+    end
+  end
+end
