d06ca8e5be655913f3c1b63e5b24617db661922b
Caleb Thomas <cjthomas00@comcast.net>
2023-04-24 11:43:18 -0600

    refactor salary controller to bring index to 5 lines.

--- app/controllers/api/v0/salaries_controller.rb
+++ app/controllers/api/v0/salaries_controller.rb
@@ -3,9 +3,8 @@ class Api::V0::SalariesController < ApplicationController
   def index 
     name = params[:destination]
     destination = LocationFacade.get_location(params[:destination])
-    forecast = WeatherFacade.get_forecasted_weather(destination)
-    weather = forecast.current_weather
+    forecast = WeatherFacade.get_forecasted_weather(destination).current_weather
     salaries = SalaryFacade.get_salaries(params[:destination])
-    render json: SalarySerializer.new(name, salaries, weather).salaries_serializer
+    render json: SalarySerializer.new(name, salaries, forecast).salaries_serializer
   end
 end
