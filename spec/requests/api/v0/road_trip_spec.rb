24ba25969559bf6a8627730ec8e29c72789e392b
Caleb Thomas <cjthomas00@comcast.net>
2023-04-25 15:24:26 -0600

    add sad path, edge case, and long trip

--- spec/requests/api/v0/road_trip_spec.rb
+++ spec/requests/api/v0/road_trip_spec.rb
@@ -9,24 +9,123 @@ RSpec.describe "Road Trip Creation", :vcr do
       "destination": "Grand Junction,CO",
       "api_key": "jgn983hy48thw9begh98h4539h4"
       }
+      post "/api/v0/road_trip", params: body, headers: { "Content_Type" => "application/json", "Accept" => "application/json" }, as: :json
+      expect(response.status).to eq(201)
+      expect(response.content_type).to eq("application/json; charset=utf-8")
+
+      parsed_data = JSON.parse(response.body, symbolize_names: true)
+
+      expect(parsed_data).to be_a(Hash)
+      expect(parsed_data[:data].keys).to eq([:id, :type, :attributes])
+      expect(parsed_data[:data][:id]).to eq(nil)
+      expect(parsed_data[:data][:type]).to eq("road_trip")
+      expect(parsed_data[:data][:attributes].keys).to eq([:start_city, :end_city, :travel_time, :weather_at_eta])
+      expect(parsed_data[:data][:attributes][:start_city]).to eq("Loveland, CO")
+      expect(parsed_data[:data][:attributes][:end_city]).to eq("Grand Junction, CO")
+      expect(parsed_data[:data][:attributes][:travel_time]).to eq("04:26:14")
+      expect(parsed_data[:data][:attributes][:weather_at_eta].keys).to eq([:datetime, :temperature, :condition])
+    end
+  end
+
+  describe "happy path long trip" do
+    it "can create a new road trip and return a 201 status code" do
+      user = User.create!(email: "getmail@gmail.com", password: "password", password_confirmation: "password", api_key: "jgn983hy48thw9begh98h4539h4")
+      body = {  
+      "origin": "New York,NY",
+      "destination": "Los Angeles,CA",
+      "api_key": "jgn983hy48thw9begh98h4539h4"
+    }
     post "/api/v0/road_trip", params: body, headers: { "Content_Type" => "application/json", "Accept" => "application/json" }, as: :json
     expect(response.status).to eq(201)
     expect(response.content_type).to eq("application/json; charset=utf-8")
-
+    
     parsed_data = JSON.parse(response.body, symbolize_names: true)
-
+  
     expect(parsed_data).to be_a(Hash)
     expect(parsed_data[:data].keys).to eq([:id, :type, :attributes])
     expect(parsed_data[:data][:id]).to eq(nil)
     expect(parsed_data[:data][:type]).to eq("road_trip")
     expect(parsed_data[:data][:attributes].keys).to eq([:start_city, :end_city, :travel_time, :weather_at_eta])
-    expect(parsed_data[:data][:attributes][:start_city]).to eq("Loveland, CO")
-    expect(parsed_data[:data][:attributes][:end_city]).to eq("Grand Junction, CO")
-    expect(parsed_data[:data][:attributes][:travel_time]).to eq("04:26:14")
+    expect(parsed_data[:data][:attributes][:start_city]).to eq("New York, NY")
+    expect(parsed_data[:data][:attributes][:end_city]).to eq("Los Angeles, CA")
+    expect(parsed_data[:data][:attributes][:travel_time]).to eq("40:15:59")
     expect(parsed_data[:data][:attributes][:weather_at_eta].keys).to eq([:datetime, :temperature, :condition])
-    expect(parsed_data[:data][:attributes][:weather_at_eta][:datetime]).to eq("04-25-2023 03:00 PM")
-    expect(parsed_data[:data][:attributes][:weather_at_eta][:temperature]).to eq(49.5)
-    expect(parsed_data[:data][:attributes][:weather_at_eta][:condition]).to eq("Patchy light rain with thunder")
+    end
+  end
+  
+  describe "sad path" do
+    it "returns an error if the api key is incorrect" do
+      user = User.create!(email: "getmail@gmail.com", password: "password", password_confirmation: "password", api_key: "jgn983hy48thw9begh98h4539h4")
+      body = {  
+      "origin": "Loveland,CO",
+      "destination": "Grand Junction,CO",
+      "api_key": "wrong_api_key"
+      }
+      post "/api/v0/road_trip", params: body, headers: { "Content_Type" => "application/json", "Accept" => "application/json" }, as: :json
+      expect(response.status).to eq(401)
+      expect(response.content_type).to eq("application/json; charset=utf-8")
+      parsed_data = JSON.parse(response.body, symbolize_names: true)
+      expect(parsed_data[:error]).to eq("Unauthorized")
+    end
+
+    it "returns an error if the api key is missing" do
+      user = User.create!(email: "getmail@gmail.com", password: "password", password_confirmation: "password", api_key: "jgn983hy48thw9begh98h4539h4")
+      body = {  
+      "origin": "Loveland,CO",
+      "destination": "Grand Junction,CO",
+      "api_key": ""
+      }
+      post "/api/v0/road_trip", params: body, headers: { "Content_Type" => "application/json", "Accept" => "application/json" }, as: :json
+      expect(response.status).to eq(401)
+      expect(response.content_type).to eq("application/json; charset=utf-8")
+      parsed_data = JSON.parse(response.body, symbolize_names: true)
+      expect(parsed_data[:error]).to eq("Missing Parameters, Unable to Process")
+    end
+
+    it "returns an error if the origin is missing" do
+      user = User.create!(email: "getmail@gmail.com", password: "password", password_confirmation: "password", api_key: "jgn983hy48thw9begh98h4539h4")
+      body = {  
+      "origin": "",
+      "destination": "Grand Junction,CO",
+      "api_key": "jgn983hy48thw9begh98h4539h4"
+      }
+      post "/api/v0/road_trip", params: body, headers: { "Content_Type" => "application/json", "Accept" => "application/json" }, as: :json
+      expect(response.status).to eq(401)
+      expect(response.content_type).to eq("application/json; charset=utf-8")
+      parsed_data = JSON.parse(response.body, symbolize_names: true)
+      expect(parsed_data[:error]).to eq("Missing Parameters, Unable to Process")
+    end
+
+    it "returns an error if the destination is missing" do
+      user = User.create!(email: "getmail@gmail.com", password: "password", password_confirmation: "password", api_key: "jgn983hy48thw9begh98h4539h4")
+      body = {  
+      "origin": "Loveland,CO",
+      "destination": "",
+      "api_key": "jgn983hy48thw9begh98h4539h4"
+      }
+      post "/api/v0/road_trip", params: body, headers: { "Content_Type" => "application/json", "Accept" => "application/json" }, as: :json
+      expect(response.status).to eq(401)
+      expect(response.content_type).to eq("application/json; charset=utf-8")
+      parsed_data = JSON.parse(response.body, symbolize_names: true)
+      expect(parsed_data[:error]).to eq("Missing Parameters, Unable to Process")
+    end
+  end
+  
+  describe "edge case" do
+    it "returns a message for impossible trip" do
+      user = User.create!(email: "getmail@gmail.com", password: "password", password_confirmation: "password", api_key: "jgn983hy48thw9begh98h4539h4")
+      
+      body = {  
+      "origin": "Loveland,CO",
+      "destination": "London,UK",
+      "api_key": "jgn983hy48thw9begh98h4539h4"
+      }
+      post "/api/v0/road_trip", params: body, headers: { "Content_Type" => "application/json", "Accept" => "application/json" }, as: :json
+      expect(response.status).to eq(201)
+      expect(response.content_type).to eq("application/json; charset=utf-8")
+      parsed_data = JSON.parse(response.body, symbolize_names: true)
+      expect(parsed_data[:data][:attributes][:travel_time]).to eq("impossible")
+      expect(parsed_data[:data][:attributes][:weather_at_eta]).to eq({})
     end
   end
 end
